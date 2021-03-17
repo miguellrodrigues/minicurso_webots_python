d25b015717eb98fb4729199a8ddede35c866e573
Miguel <miguellukas52@gmail.com>
2021-03-17 11:00:51 -0300

    .

--- aula-1-sincrona.py
+++ aula-1-sincrona.py
@@ -13,7 +13,7 @@ raio_c = diametro_c / 2.0
 motores = []
 encoders = []
 
-ultima_posicao = [.0, .0]
+ultima_posicao = [.0, .0, .0, .0]
 
 
 for i in range(1, 5):
@@ -23,7 +23,7 @@ for i in range(1, 5):
 
 for motor in motores:
     motor.setPosition(float('inf'))
-    motor.setVelocity(2.0)
+    motor.setVelocity(.0)
 
 
 for encoder in encoders:
@@ -43,17 +43,18 @@ def virar(angulo):
     x = (pi * raio_c * angulo) / 180.0
 
     if x > 0:
-        while leitura_encoder(0) - ultima_posicao[0] < x:
+        while leitura_encoder(2) - ultima_posicao[2] < x:
             setar_velocidades([2, -2, 2, -2])
 
             supervisor.step(16)
     else:
-        while leitura_encoder(0) - ultima_posicao[0] > x:
+        while leitura_encoder(1) - ultima_posicao[1] < -x:
             setar_velocidades([-2, 2, -2, 2])
 
             supervisor.step(16)
 
-    ultima_posicao[0] = leitura_encoder(0)
+    ultima_posicao[2] = leitura_encoder(2)
+    ultima_posicao[1] = leitura_encoder(1)
 
 
 while supervisor.step(16) != -1:
